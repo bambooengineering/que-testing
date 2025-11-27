b7879eabf836a02a467b301c42b17b972c828c04
Owen Stephens <owen@bambooloans.com>
2025-11-27 13:21:37 +0000

    Support Que.bulk_enqueue

--- lib/que/testing/connection_pool.rb
+++ lib/que/testing/connection_pool.rb
@@ -3,16 +3,41 @@ require_relative "job_params"
 module Que
   module Testing
     class ConnectionPool < Que::ConnectionPool
-      def execute(command, params = [])
-        return [] unless command == :insert_job
+      def checkout
+        yield
+      end
 
+      def in_transaction?
+        true
+      end
+
+      def execute(command, params = [])
+        if command == :insert_job
+          insert_job(params)
+
+          params
+        elsif command == :bulk_insert_jobs
+          queue, priority, run_at, job_class, args_and_kwargs, data = params
+
+          JSON.parse(args_and_kwargs).each do |hash|
+            args, kwargs = hash.values_at("args", "kwargs").map { |x| JSON.dump(x) }
+
+            insert_job([queue, priority, run_at, job_class, args, kwargs, data])
+          end
+
+          params
+        else
+          []
+        end
+      end
+
+      private def insert_job(params)
         job = JobParams.new(*params)
         klass = class_for(job.job_class)
         jobs[klass] << job
-        params
       end
 
-      def class_for(str)
+      private def class_for(str)
         str.split('::').reduce(Object, &:const_get)
       end
 
